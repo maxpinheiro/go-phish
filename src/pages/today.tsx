646030f5e741a61661e8e5107487dfbf93f0c63a
Max Pinheiro <maxpinheiro181@gmail.com>
2023-12-28 12:40:42 -0500

    use theme color for 404 icon

--- src/pages/today.tsx
+++ src/pages/today.tsx
@@ -1,12 +1,11 @@
 import NotFoundIcon from '@/media/404.svg';
 import { ResponseStatus } from '@/types/main';
 import { getTodaysShow } from '@/services/show.service';
-import { Run, Show } from '@prisma/client';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
-import { getRunById, getRunWithVenue } from '@/services/run.service';
+import { getRunWithVenue } from '@/services/run.service';
 import moment from 'moment-timezone';
 import ErrorMessage from '@/components/shared/ErrorMessage';
 import { RunWithVenue } from '@/models/run.model';
@@ -46,7 +45,7 @@ export const getServerSideProps: GetServerSideProps<TodayPageProps> = async (con
 };
 
 const Today: React.FC<TodayPageProps> = ({ error, notFound, show, run, dateStr }) => {
-  const { color } = useThemeContext();
+  const { color, hexColor } = useThemeContext();
   if (error || !dateStr) return <ErrorMessage error={error} />;
 
   return (
@@ -76,7 +75,7 @@ const Today: React.FC<TodayPageProps> = ({ error, notFound, show, run, dateStr }
         </div>
         {notFound || !show || !run ? (
           <div className="flex flex-col items-center space-y-5 py-5">
-            <NotFoundIcon className="mt-2.5" />
+            <NotFoundIcon className="mt-2.5" hexColor={hexColor} />
             <p>Sorry! There is no show today.</p>
             <Link href="/shows">View All Shows</Link>
             <a href="https://phish.com/tours/" target="_blank">
