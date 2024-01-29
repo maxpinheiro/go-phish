ef3755f7b2d05a0f57c2de55e514efc41113fbea
Max Pinheiro <maxpinheiro181@gmail.com>
2024-01-28 21:36:45 -0500

    basic 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import { useThemeContext } from '@/store/theme.store';
+
+export default function Custom500Page() {
+  const { color } = useThemeContext();
+  return (
+    <>
+      <Head>
+        <title>Site Maintenance | GoPhish</title>
+      </Head>
+      <div className="flex flex-col items-center px-4">
+        <p className={`text-4xl text-${color} font-semibold mt-12 mb-8`}>Site Maintenance</p>
+        <p className="text-md text-center">
+          We&apos;re all in the bathtub now, making Bathub Gin. We will be back soon!
+        </p>
+        <a href="https://phish.com/tours/" className={`text-lg text-${color} font-medium my-6`}>
+          View Upcoming Tours
+        </a>
+      </div>
+    </>
+  );
+}
