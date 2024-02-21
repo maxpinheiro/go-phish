75ba6e9d88f7d24fee580bc60b9a3f00f01b4a41
Max Pinheiro <maxpinheiro181@gmail.com>
2024-02-21 11:38:41 -0500

    update text

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -12,7 +12,7 @@ export default function Custom500Page() {
       <div className="flex flex-col items-center px-4">
         <p className={`text-4xl text-${color} font-semibold mt-12 mb-8`}>Site Maintenance</p>
         <p className="text-md text-center">
-          We&apos;re all in the bathtub now, making Bathub Gin. We will be back soon!
+          We&apos;re all in the bathtub now, making some Bathub Gin. We will be back soon!
         </p>
         <a href="https://phish.com/tours/" className={`text-lg text-${color} font-medium my-6`}>
           View Upcoming Tours
